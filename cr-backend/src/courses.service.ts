76192e94a114de1c2a6bc239a95a45a90cecffa0
Bigtourlantaop <wichanon.wic@gmail.com>
2024-04-09 15:47:43 +0700

    uses Courses Service for finding Courses in backend

---
+++ cr-backend/src/courses.service.ts
@@ -0,0 +1,22 @@
+import { Course } from "./interfaces/course.interface";
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class CoursesService {
+    async findAll(): Promise<Course[]> {
+        return [
+            { id: '100',
+              number: '01204111',
+              title: 'Computer and Programming'
+            },
+            { id: '213fds',
+              number: '01204211',
+              title: 'Discrete mathematics and linear algebra'
+            },
+            { id: 'fljdksjfoiwr',
+              number: '01204313',
+              title: 'Design and analysis of algorithms'
+            },
+          ];
+    }
+}
